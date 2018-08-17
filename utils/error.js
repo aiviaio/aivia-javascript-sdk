9820d22ae74b6e2700b00013027d905dda14c5bc
dshm <dima.shmalko@gmail.com>
2018-08-17 09:40:23 +0300

    fix test

--- utils/error.js
+++ utils/error.js
@@ -1,12 +1,15 @@
-exports.shouldRevert = error => {
-  assert(
-    error.message === "VM Exception while processing transaction: revert",
-    "error message must be 'VM Exception while processing transaction: revert'"
-  );
-};
-exports.isError = error => {
-  assert(
-    error.message === "VM Exception while processing transaction: revert",
-    "error message must be 'VM Exception while processing transaction: revert'"
-  );
+module.exports = {
+  shouldRevert: error => {
+    assert(
+      error.message === "VM Exception while processing transaction: revert",
+      "error message must be 'VM Exception while processing transaction: revert'"
+    );
+  },
+
+  shouldThrowArgError: error => {
+    assert(
+      error.message === "Invalid number of arguments to Solidity function",
+      "error message must be 'Invalid number of arguments to Solidity function'"
+    );
+  }
 };
