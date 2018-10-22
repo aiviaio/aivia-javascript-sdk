645887525d9aaf3abcaeef4825870ae6b0630c8b
dshm <dima.shmalko@gmail.com>
2018-10-22 18:09:41 +0300

    .

--- sdk/src/contracts/Project.js
+++ sdk/src/contracts/Project.js
@@ -23,6 +23,8 @@ const deployProject = async (
   },
   { from }
 ) => {
+  console.log(custodianAddress);
+  console.log("0x09B4A049763b08cD907C8Fbe9c52580E6a4Cc96e");
   await errorHandler(
     proxy.methods
       .deployProject(
@@ -44,10 +46,16 @@ const deployProject = async (
         ],
         custodianAddress
       )
-      .send({ from })
+      .send({
+        from,
+        gasPrice: "10000000000000",
+        gas: 6721975
+      })
   );
 };
 
 module.exports = {
   deployProject
 };
+
+// app.deployProject(["0x5468697320697320746573742070726f6a656374","0x45575420546f6b656e", "0x455754"], [2, 1000, 100, "100000000000000000"], ["200000000000000000", "2200000000000000000", "1100000000000000000"], "0x09B4A049763b08cD907C8Fbe9c52580E6a4Cc96e", {from: "0xf0bfe92ab08A01e75a83E4f226b311894F068871"});
