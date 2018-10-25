f840f4f0ab23cab5d56dc34d32c7564367c67acf
dshm <dima.shmalko@gmail.com>
2018-10-25 14:13:13 +0300

    project Registry SDK

---
+++ sdk/src/contracts/ProjectRegistry.js
@@ -0,0 +1,52 @@
+const is = require("is_js");
+const ProjectsRegistry = require("../ABI/ProjectsRegistry");
+const web3 = require("../core");
+const errorHandler = require("../helpers/errorHandler");
+const Error = require("../helpers/Error");
+
+const projectRegistry = new web3.eth.Contract(
+  ProjectsRegistry.abi,
+  ProjectsRegistry.address
+);
+
+// get project ID by project address
+const getProjectID = async projectAddress => {
+  if (!web3.utils.isAddress(projectAddress)) {
+    return Error({
+      name: "params",
+      message: "'projectAddress' field must be a number"
+    });
+  }
+
+  const ID = await errorHandler(
+    projectRegistry.methods.getProjectID(projectAddress).call()
+  );
+  return ID;
+};
+// get all deployed components
+const getProjectByID = async ID => {
+  if (is.not.number(ID)) {
+    return Error({
+      name: "params",
+      message: "'ID' field must be a number"
+    });
+  }
+  const ProjectAddress = await errorHandler(
+    projectRegistry.methods.getProjectByID(ID).call()
+  );
+  return ProjectAddress;
+};
+
+// get project list
+const getProjectList = async () => {
+  const projectList = await errorHandler(
+    projectRegistry.methods.getProjectList().call()
+  );
+  return projectList;
+};
+
+module.exports = {
+  getProjectID,
+  getProjectByID,
+  getProjectList
+};
