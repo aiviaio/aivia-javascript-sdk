121d33a663f9bc3113e6564f443b0798ee2091fd
dshm <dima.shmalko@gmail.com>
2018-11-28 20:10:46 +0200

    fix error

--- sdk/src/components/Ratings.js
+++ sdk/src/components/Ratings.js
@@ -5,13 +5,12 @@ const errorHandler = require("../helpers/errorHandler");
 const getRatingsList = async () => {
   const addressesList = await errorHandler(AssetsRegistry.getAssetsList());
 
-  const tokensList = await addressesList.map(async ({ address }) => {
+  const tokensList = addressesList.map(async ({ address }) => {
     const symbol = await AssetsRegistry.getAssetSymbol(address);
     const price = await Asset.getAssetPrice(address);
     const investors = Number(await Asset.getInvestorsCount(address));
-    return { symbol, address, price, investors };
+    return { symbol, address, investors, price };
   });
-
   return Promise.all(tokensList);
 };
 module.exports = {
