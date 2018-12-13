4b6c102df7bdf7b611d1b5b2c73b136dbc2cf89c
dshm <dima.shmalko@gmail.com>
2018-12-13 16:15:33 +0200

    add estimate const

--- sdk/src/components/Deployer.js
+++ sdk/src/components/Deployer.js
@@ -1,8 +1,12 @@
 const Proxy = require("../ABI/Proxy");
 const AssetsRegistry = require("./AssetsRegistry");
+const ERC20 = require("./ERC20");
+const config = require("../config");
+const utils = require("../utils");
 const signedTX = require("../helpers/signedTX");
 const { createInstance } = require("../helpers/createInstance");
 const { errorHandler, isObject, isInteger, isZeroAddress } = require("../helpers/errorHandler");
+const Error = require("../helpers/Error");
 const EntryPoint = require("./EntryPoint");
 const ReselectData = require("../projects/ReselectData");
 
@@ -10,8 +14,16 @@ const deployProject = async (type, params, options, callback) => {
   isInteger({ type });
   isObject({ params, options });
   const { tokenSymbol } = params.tokenDetails;
-
+  const gasPrice = options.gasPrice || config.get("DEFAULT_GAS_PRICE");
+  const approximateCost = utils.fromWei(gasPrice * 8000000);
+  const balance = await ERC20.getBalance(options.from);
   const tokenAddress = await AssetsRegistry.getAssetAddress(tokenSymbol);
+  if (balance < approximateCost) {
+    Error({
+      name: "transaction",
+      message: `For deployment, the balance must be at least ${approximateCost} ETH`
+    });
+  }
   isZeroAddress({
     tokenAddress: {
       value: tokenAddress,
@@ -23,7 +35,6 @@ const deployProject = async (type, params, options, callback) => {
   const _params = ReselectData.input(type, params);
   const deployAction = instance.methods.deployProject(..._params);
   const initAction = instance.methods.initProject();
-
   await errorHandler(
     signedTX({
       data: deployAction.encodeABI(),
