eb502040e699bd4d7d87577a163f82cca1efc91e
dshm <dima.shmalko@gmail.com>
2019-01-11 14:43:52 +0200

    change structure

--- sdk/test/core.js
+++
@@ -1,7 +0,0 @@
-const AIVIA_SDK = require("../src");
-const ENTRY_POINT =
-  require("../src/ABI/EntryPoint").address || process.env.ENTRY_POINT;
-
-const SDK = new AIVIA_SDK(ENTRY_POINT, "http://127.0.0.1:8545");
-
-module.exports = SDK;
