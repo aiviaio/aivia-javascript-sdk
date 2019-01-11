5cd57e77a82460980feba1c693aa3e2ae0c9b441
dshm <dima.shmalko@gmail.com>
2019-01-11 17:01:31 +0200

    add licence

--- docs.js
+++ docs.js
@@ -1,6 +1,23 @@
 const generator = require("jsdoc-to-markdown");
 const fs = require("fs");
 
+const title = "# [AIVIA JavaScript SDK](https://aivia.io/)";
+const badges = `
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/dshm/aivia-sdk/LICENSE)
+[![npm version](https://img.shields.io/npm/v/aivia.svg?style=flat)](https://www.npmjs.com/package/aivia)
+`;
+
+const install = `
+### npm
+npm install aivia -S
+`;
+
+const head = `
+${title}
+${badges}
+${install}
+`;
+
 generator.render({ files: "src/**/*.js", separators: true }).then(docs => {
-  fs.writeFile("./README.md", docs, () => {});
+  fs.writeFile("./README.md", head + docs, () => {});
 });
